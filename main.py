7e0ea0ecb808c5b13a8a317025265d4a0b8a39f8
lylabrick <lauradev61@gmail.com>
2026-04-16 21:28:36 -0300

    Added decorators memorize and timer

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from utils import flatten_ejercicio1
 
 print(flatten_ejercicio1([1, [2, [3, [4]], 5]]))
 print(flatten_ejercicio1(["primero", ["segundo", ["tercero", ["cuarto"]], "quinto"]]))
+print(flatten_ejercicio1([1, [2, [3, [4]], 5]]))
--- utils/flat.py
+++ utils/flat.py
@@ -1,9 +1,53 @@
-def flatten_ejercicio1(listOfLists):
+import time
+import functools
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        inicio = time.perf_counter()
+        print(f"Este es el valor del time inicio {inicio}"  )
+        resultado = func(*args)
+        fin = time.perf_counter()
+        tiempo = fin - inicio
+        print(f"Tiempo de ejecución de {func.__name__}: {tiempo:.6f} segundos")
+        return resultado
+    return wrapper
+
+def memorize(func):
+    cache = {}
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        # convertir args a algo hasheable porque las listas no lo son 
+        key = str(args)
+
+        if key not in cache:
+            print(f"     [Calculado] {func.__name__} con input {args}")
+            cache[key] = func(*args)
+        else:
+            print(f".    [Cache hit] {func.__name__} con input {args}")
+        return cache[key]
+    
+    wrapper.cache = cache
+    wrapper.clear_cache = lambda: cache.clear()
+
+    return wrapper
+
+
+def _flatten_ejercicio1(listOfLists):
     resultado = []
     for lista in listOfLists:
         if isinstance(lista, list):
-            resultado += flatten_ejercicio1(lista)
+            resultado += _flatten_ejercicio1(lista)
         else:
             resultado.append(lista)
 
     return resultado
+
+
+@timer
+@memorize
+def flatten_ejercicio1(listOfLists):
+    return _flatten_ejercicio1(listOfLists)
+
+
